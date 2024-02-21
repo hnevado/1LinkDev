930a7812820b9c5e4b1263475bd11ed2e8612c6d
Hector Nevado <hectornevado46@gmail.com>
2024-02-22 00:48:16 +0100

    Cambio en la maquetación de la navegación

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,8 @@
-<div class="py-2 border-gray-800 border-t space-x-3 px-2 items-center flex justify-center pt-6 pb-6">
-    <a href="#" class="text-4xl font-extrabold mb-4 text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">1LinkDev</a>
+<div class="max-w-md mx-auto pt-8 pb-8">
+    <div class="flex justify-center items-center">
+        <div class="w-1/2 h-px bg-purple-500"></div>
+        <a href="#" class="text-4xl font-extrabold mb-4 text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500 mx-4"> 1LinkDev </a>
+        <div class="w-1/2 h-px bg-purple-500"></div>
+    </div>
 </div>
-
     
