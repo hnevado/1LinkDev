40e8245b256de8e91b6a4ee2ec32ef1e964a1700
Hector Nevado <hectornevado46@gmail.com>
2024-02-26 19:29:14 +0100

    Añadido on update cascade y ondelete cascade a la tabla links

--- database/migrations/2024_02_25_160954_create_links_table.php
+++ database/migrations/2024_02_25_160954_create_links_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('links', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->text('link');
             $table->text('description');
 
